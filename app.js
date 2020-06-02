77151d099ed412670e1bb21a1ab40b75742df8d7
Adam Brown <adam.edward.brown@gmail.com>
2020-06-02 11:45:46 -0400

    Completed Assignment

---
+++ app.js
@@ -0,0 +1,33 @@
+const playstore = require('./playstore.js');
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+app.use(morgan('combined'));
+app.listen(8080,()=>console.log('Server running on 8080'));
+
+let resultList = [...playstore];
+
+app.get('/apps',(req,res)=>{
+  const {sort,genres} = req.query;
+  //sort optional, rating/app
+  if(['Rating','App'].includes(sort)){
+    resultList.sort((a,b)=>
+      a[sort].toLowerCase()<b[sort].toLowerCase()?-1:1
+    );
+  } else if (sort) {
+    res.status(400).send('Please provide a sort value in Proper format');
+    return;
+  }
+  
+  //genre optional, filter by given
+  const genreList = ['Action','Puzzle','Strategy','Casual','Arcade','Card'];
+  
+  if(genres && genreList.includes(genres)){
+    resultList = resultList.filter(app=>app.Genres===genres);
+  } else if (genres){
+    res.status(400).send('Please provide a genre value in Proper format');
+    return;
+  }
+
+  res.json(resultList);
+});
